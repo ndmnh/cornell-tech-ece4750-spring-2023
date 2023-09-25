1f17e88039f6a375cdac5addf1cb3c95b4f2a97b
Minh Nguyen <ndminh208@gmail.com>
2023-09-25 01:47:00 -0400

    feat: add basic files for lab1

---
+++ lab1/kernel/functional.c
@@ -0,0 +1,30 @@
+#include "functional.h"
+
+float relu(float x)
+{
+    return x > 0 ? x : 0;
+}
+
+void applyRelu(float *input, int inputSize)
+{
+    for (int i = 0; i < inputSize; i++)
+    {
+        input[i] = relu(input[i]);
+    }
+}
+
+float *softmax(float *input, int inputSize)
+{
+    float *softmaxOutput = malloc(inputSize * sizeof(*softmaxOutput));
+    float sum = 0.0;
+
+    for (int i = 0; i < inputSize; i++) {
+        softmaxOutput[i] = expf(input[i]);
+        sum += softmaxOutput[i];
+    }
+    for (int i = 0; i < inputSize; i++) {
+        softmaxOutput[i] /= sum;
+    }
+
+    return softmaxOutput;
+}
