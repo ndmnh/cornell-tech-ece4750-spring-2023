d71be2f95d6f96d63ac1ac46fb581772f993abe3
Minh Nguyen <ndminh208@gmail.com>
2023-10-13 16:21:36 -0400

    feat: complete matmul sparse

--- lab3/matrix_ops.c
+++ lab3/matrix_ops.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include "matrix_ops.h"
 
-int BLK_SIZE = 32;
-
 typedef struct {
     int *row_pointers;
     int *column_indices;
@@ -115,39 +113,6 @@ float **matmul(float **A, float **B, int A_rows, int A_cols, int B_rows, int B_c
         for (int j = 0; j < B_cols; j++) {
             for (int k = 0; k < B_rows; k++) {
                 C[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    return C;
-}
-
-
-float **matmul_blocking(float **A, float **B, int A_rows, int A_cols, int B_rows, int B_cols) {
-    if (A_cols != B_rows) {
-        printf("Matrix dimensions incompatible for multiplication.\n");
-        return NULL;
-    }
-
-    float **C = (float **)malloc(A_rows * sizeof(float *));
-    for (int i = 0; i < A_rows; i++) {
-        C[i] = (float *)malloc(B_cols * sizeof(float));
-        for (int j = 0; j < B_cols; j++) {
-            C[i][j] = 0;
-        }
-    }
-
-    for (int ii = 0; ii < A_rows; ii += BLK_SIZE) {
-        for (int jj = 0; jj < B_cols; jj += BLK_SIZE) {
-            for (int kk = 0; kk < A_cols; kk += BLK_SIZE) {
-                // Multiply the tiles
-                for (int i = ii; i < ii + BLK_SIZE && i < A_rows; i++) {
-                    for (int j = jj; j < jj + BLK_SIZE && j < B_cols; j++) {
-                        for (int k = kk; k < kk + BLK_SIZE && k < A_cols; k++) {
-                            C[i][j] += A[i][k] * B[k][j];
-                        }
-                    }
-                }
             }
         }
     }
